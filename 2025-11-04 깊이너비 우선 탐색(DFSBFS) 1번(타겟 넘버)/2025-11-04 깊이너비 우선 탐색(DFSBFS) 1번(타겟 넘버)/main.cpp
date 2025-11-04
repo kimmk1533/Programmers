11bf56381b7cb6ab26795d0df8828007da4ade13
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-05 01:09:06 +0900

    2025-11-04 깊이/너비 우선 탐색(DFS/BFS) 1번(타겟 넘버)

---
+++ 2025-11-04 깊이너비 우선 탐색(DFSBFS) 1번(타겟 넘버)/2025-11-04 깊이너비 우선 탐색(DFSBFS) 1번(타겟 넘버)/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int dfs(vector<int>& numbers, int target, int sum, int i)
+{
+	int answer = 0;
+
+	if (i >= numbers.size() - 1)
+	{
+		if (sum + numbers[i] == target)
+			++answer;
+		if (sum - numbers[i] == target)
+			++answer;
+
+		return answer;
+	}
+
+	answer += dfs(numbers, target, sum + numbers[i], i + 1);
+	answer += dfs(numbers, target, sum - numbers[i], i + 1);
+
+	return answer;
+}
+
+int solution(vector<int> numbers, int target)
+{
+	int answer = dfs(numbers, target, 0, 0);
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(vector<int> { 4, 1, 2, 1 }, 4);
+}
