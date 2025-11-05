084de0b878256508f30121a0e7fe730ceebaf259
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-05 19:18:03 +0900

    2025-11-05 해시 2번 (폰켓몬)

---
+++ 2025-11-05 해시 2번 (폰켓몬)/2025-11-05 해시 2번 (폰켓몬)/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include <set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+	int cnt = nums.size() >> 1;
+
+	set<int> s(nums.begin(), nums.end());
+
+	return min<int>(cnt, s.size());
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution({ 3, 1, 2, 3 });
+}
