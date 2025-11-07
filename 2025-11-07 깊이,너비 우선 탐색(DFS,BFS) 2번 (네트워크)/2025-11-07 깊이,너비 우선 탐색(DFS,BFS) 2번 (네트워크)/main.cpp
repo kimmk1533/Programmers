7856b6b9a85585fb914a61a8b182951a80c46db6
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-07 19:17:59 +0900

    2025-11-07 깊이/너비 우선 탐색(DFS/BFS) 2번 (네트워크)

---
+++ 2025-11-07 깊이,너비 우선 탐색(DFS,BFS) 2번 (네트워크)/2025-11-07 깊이,너비 우선 탐색(DFS,BFS) 2번 (네트워크)/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool visited[200];
+
+void dfs(int n, vector<vector<int>>& computers, int computer)
+{
+	visited[computer] = true;
+
+	for (int i = 0; i < n; ++i)
+	{
+		// 이미 방문한 컴퓨터는 제외
+		if (visited[i] == true)
+			continue;
+		// 연결되지 않은 컴퓨터 제외
+		if (computers[computer][i] == 0)
+			continue;
+
+		dfs(n, computers, i);
+	}
+}
+
+int solution(int n, vector<vector<int>> computers)
+{
+	int answer = 0;
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (visited[i] == true)
+			continue;
+
+		dfs(n, computers, i);
+
+		++answer;
+	}
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(4,
+		{
+			{ 1, 1, 0, 1 },
+			{ 1, 1, 0, 0 },
+			{ 0, 0, 1, 1 },
+			{ 1, 0, 1, 1 },
+		});
+
+	return 0;
+}
