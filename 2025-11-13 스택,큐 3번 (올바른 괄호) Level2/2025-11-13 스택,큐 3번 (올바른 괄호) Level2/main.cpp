3efb2c30c246ba2bd4d20a33d48955a640b6a419
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-13 19:21:34 +0900

    2025-11-13 스택/큐 3번 (올바른 괄호)

---
+++ 2025-11-13 스택,큐 3번 (올바른 괄호) Level2/2025-11-13 스택,큐 3번 (올바른 괄호) Level2/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+	int indent = 0;
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if (s[i] == ')')
+		{
+			if (indent == 0)
+				return false;
+
+			--indent;
+		}
+		if (s[i] == '(')
+			++indent;
+	}
+	return indent == 0;
+}
+
+int main()
+{
+	cout << solution("()()") << "\n";
+	cout << solution("(())()") << "\n";
+	cout << solution(")()(") << "\n";
+	cout << solution("(()(") << "\n";
+	cout << solution("(") << "\n";
+
+	return 0;
+}
