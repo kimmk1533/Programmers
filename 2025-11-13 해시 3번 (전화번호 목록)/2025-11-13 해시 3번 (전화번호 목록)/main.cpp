42fc10f4eb9406de28ce17371f0cacb7db7b63e4
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-13 18:59:32 +0900

    2025-11-13 해시 3번 (전화번호 목록)

---
+++ 2025-11-13 해시 3번 (전화번호 목록)/2025-11-13 해시 3번 (전화번호 목록)/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool solution(vector<string> phone_book)
+{
+	sort(phone_book.begin(), phone_book.end());
+
+	for (int i = 0; i < phone_book.size() - 1; ++i)
+	{
+		if (phone_book[i + 1].find(phone_book[i], 0) == 0)
+			return false;
+	}
+
+	return true;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(
+		{
+			"119",
+			"97674223",
+			"1195524421",
+		}
+		) << "\n";
+
+	cout << solution(
+		{
+			"123",
+			"456",
+			"789",
+		}
+		) << "\n";
+
+	cout << solution(
+		{
+			"12",
+			"123",
+			"1235",
+			"567",
+			"88",
+		}
+		) << "\n";
+
+	return 0;
+}
