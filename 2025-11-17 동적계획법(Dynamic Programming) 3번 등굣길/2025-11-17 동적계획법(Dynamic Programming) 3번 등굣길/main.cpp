e107d4cb503ebe90d9e77549790f06c913f39d67
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-18 00:55:40 +0900

    2025-11-17 동적계획법(Dynamic Programming) 3번 등굣길

---
+++ 2025-11-17 동적계획법(Dynamic Programming) 3번 등굣길/2025-11-17 동적계획법(Dynamic Programming) 3번 등굣길/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+// 해당 위치까지 최소거리로 오는 경우의 수
+long long dp_count[101][101];
+
+int solution(int m, int n, vector<vector<int>> puddles)
+{
+	for (int i = 0; i < puddles.size(); ++i)
+		dp_count[puddles[i][1]][puddles[i][0]] = -1;
+
+	dp_count[1][1] = 1;
+	for (int y = 1; y <= n; ++y)
+	{
+		for (int x = 1; x <= m; ++x)
+		{
+			if (dp_count[y][x] == -1)
+				continue;
+
+			dp_count[y][x] += max(0ll, dp_count[y - 1][x]) + max(0ll, dp_count[y][x - 1]);
+			dp_count[y][x] %= 1000000007;
+		}
+	}
+
+	return dp_count[n][m];
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(4, 3, {
+		{ 2, 2 },
+		}) << "\n";
+
+	return 0;
+}
