5c1e9b8339a3b431c5718abf44bcf022477a2261
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-20 06:53:52 +0900

    2025-11-20 완전탐색 4번 카펫

---
+++ 2025-11-20 완전탐색 4번 카펫/2025-11-20 완전탐색 4번 카펫/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int brown, int yellow)
+{
+	int width = 2, height = 2;
+
+	while (true)
+	{
+		height = 2;
+
+		while (height <= width)
+		{
+			int yellow_carpet = (width - 2) * (height - 2);
+			int brown_carpet = width * height - yellow_carpet;
+
+			if (yellow == yellow_carpet &&
+				brown == brown_carpet)
+				return { width, height };
+
+			++height;
+		}
+
+		++width;
+	}
+}
+
+#include <iostream>
+
+int main()
+{
+	vector<int> answer = solution(24, 24);
+	cout << answer[0] << ", " << answer[1] << "\n";
+
+	return 0;
+}
