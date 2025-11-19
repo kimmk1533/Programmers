eb23cb37ec0fe8d9e5adb8a07153aecc6f20c902
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-19 18:14:03 +0900

    2025-11-19 스택/큐 4번 프로세스

---
+++ 2025-11-19 스택,큐 4번 프로세스/2025-11-19 스택,큐 4번 프로세스/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> priorities, int location)
+{
+	queue<int> q;
+	priority_queue<int> pq(priorities.begin(), priorities.end());
+	for (int priority : priorities)
+	{
+		q.push(priority);
+	}
+
+	int answer = 1;
+
+	while (q.empty() == false)
+	{
+		int process = q.front(); q.pop();
+
+		if (process == pq.top())
+		{
+			if (location == 0)
+				break;
+
+			++answer;
+			pq.pop();
+		}
+		else
+		{
+			q.push(process);
+
+			if (location == 0)
+				location = q.size();
+		}
+
+		--location;
+	}
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution({ 2, 1, 3, 2 }, 2) << "\n";
+	cout << solution({ 1, 1, 9, 1, 1, 1 }, 0) << "\n";
+
+	return 0;
+}
