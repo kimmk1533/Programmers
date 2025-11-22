65cdf42f34fc5b183129846a8cf117ffd8f616b9
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-11-22 22:17:59 +0900

    2025-11-22 깊이/너비 우선 탐색(DFS/BFS) 4번 단어 변환

---
+++ 2025-11-22 깊이,너비 우선 탐색(DFS,BFS) 4번 단어 변환/2025-11-22 깊이,너비 우선 탐색(DFS,BFS) 4번 단어 변환/main.cpp
@@ -0,0 +1,59 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+bool check(string curr, string word)
+{
+	int cnt = 0;
+	for (int i = 0; i < word.size(); ++i)
+	{
+		if (curr[i] != word[i])
+		{
+			if (++cnt >= 2)
+				break;
+		}
+	}
+	return cnt == 1;
+}
+
+int solution(string begin, string target, vector<string> words)
+{
+	queue<pair<string, int>> wordQ;
+	unordered_map<string, bool> visited;
+	pair<string, int> curr;
+
+	wordQ.push({ begin, 0 });
+	while (wordQ.empty() == false)
+	{
+		curr = wordQ.front(); wordQ.pop();
+
+		if (curr.first == target)
+			return curr.second;
+
+		for (string word : words)
+		{
+			if (visited[word])
+				continue;
+
+			if (check(curr.first, word))
+			{
+				wordQ.push({ word, curr.second + 1 });
+				visited[word] = true;
+			}
+		}
+	}
+
+	return 0;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" }) << "\n";
+	cout << solution("hit", "cog", { "hot", "dot", "dog", "lot", "log" }) << "\n";
+
+	return 0;
+}
