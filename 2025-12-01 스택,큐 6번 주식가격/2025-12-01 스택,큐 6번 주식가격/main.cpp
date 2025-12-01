f6e09e1249a51e00fdc4f9338813a730881d4bfa
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-12-01 16:13:00 +0900

    2025-12-01 스택/큐 6번 주식가격

---
+++ 2025-12-01 스택,큐 6번 주식가격/2025-12-01 스택,큐 6번 주식가격/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> prices)
+{
+	vector<int> answer;
+
+	for (int i = 0; i < prices.size(); ++i)
+	{
+		int price = 0;
+		for (int j = i + 1; j < prices.size(); ++j)
+		{
+			++price;
+
+			if (prices[i] > prices[j])
+				break;
+		}
+		answer.push_back(price);
+	}
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	for (int answer : solution({ 1, 2, 3, 2, 3 }))
+	{
+		cout << answer << " ";
+	}
+
+	return 0;
+}
