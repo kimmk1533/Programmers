cb14f59e1fca9391af3a00ed6dc56fcb5388e27b
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-12-10 19:09:36 +0900

    2025-12-10 연습문제 (최솟값 만들기)

---
+++ 2025-12-10 연습문제 (최솟값 만들기)/2025-12-10 연습문제 (최솟값 만들기)/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+	sort(A.begin(), A.end(), less<int>());
+	sort(B.begin(), B.end(), greater<int>());
+
+	int answer = 0;
+
+	for (int i = 0; i < A.size(); ++i)
+	{
+		answer += A[i] * B[i];
+	}
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+
+
+	return 0;
+}
