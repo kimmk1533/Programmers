49af31bf3ac40f91395d2d026b95e41b3848bb93
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-12-15 20:29:55 +0900

    2025-12-15 Summer/Winter Coding(~2018) (영어 끝말잇기)

---
+++ 2025-12-15 Summer,Winter Coding(~2018) (영어 끝말잇기)/2025-12-15 Summer,Winter Coding(~2018) (영어 끝말잇기)/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+vector<int> solution(int n, vector<string> words)
+{
+	set<string> s;
+	for (int index = 0; index < words.size(); ++index)
+	{
+		if (s.insert(words[index]).second == false ||
+			(index > 0 && words[index - 1].back() != words[index].front()))
+			return { index % n + 1, index / n + 1 };
+	}
+
+	return { 0, 0 };
+}
+
+#include <iostream>
+
+int main()
+{
+	for (int i : solution(3, { "tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank" }))
+		cout << i << " ";
+
+	return 0;
+}
