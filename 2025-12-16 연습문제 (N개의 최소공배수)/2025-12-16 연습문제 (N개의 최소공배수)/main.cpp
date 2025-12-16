31ec06d97a236e50c44d347110f53d54e029803b
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-12-16 20:16:34 +0900

    2025-12-16 연습문제 (N개의 최소공배수)

---
+++ 2025-12-16 연습문제 (N개의 최소공배수)/2025-12-16 연습문제 (N개의 최소공배수)/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+// 최대 공약수
+int gcd(int a, int b)
+{
+	while (b != 0)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+// 최소 공배수
+int lcm(int a, int b)
+{
+	return a * b / gcd(a, b);
+}
+
+int solution(vector<int> arr)
+{
+	int answer = arr[0];
+	for (int i = 1; i < arr.size(); ++i)
+		answer = lcm(answer, arr[i]);
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution({ 2, 6, 8, 14 }) << endl;
+	cout << solution({ 1, 2, 3 }) << endl;
+
+	return 0;
+}
