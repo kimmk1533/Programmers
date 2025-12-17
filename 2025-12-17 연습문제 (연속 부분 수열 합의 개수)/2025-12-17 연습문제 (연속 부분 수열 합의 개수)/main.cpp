7c39a8e8704243f38a7e5840fadc7de74bf6ed60
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-12-17 18:53:10 +0900

    2025-12-17 연습문제 (연속 부분 수열 합의 개수)

---
+++ 2025-12-17 연습문제 (연속 부분 수열 합의 개수)/2025-12-17 연습문제 (연속 부분 수열 합의 개수)/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+int solution(vector<int> elements)
+{
+	set<int> s;
+
+	for (int size = 1; size <= elements.size(); ++size)
+	{
+		for (int startIndex = 0; startIndex < elements.size(); ++startIndex)
+		{
+			int sum = 0;
+			for (int sumIndex = 0; sumIndex < size; ++sumIndex)
+			{
+				sum += elements[(startIndex + sumIndex) % elements.size()];
+			}
+			s.insert(sum);
+		}
+	}
+
+	return s.size();
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution({ 7, 9, 1, 1, 4 });
+
+	return 0;
+}
