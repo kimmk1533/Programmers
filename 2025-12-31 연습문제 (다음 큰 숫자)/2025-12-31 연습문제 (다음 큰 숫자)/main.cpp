2bf63557beb535c9cbb02638f67253913c05fe1b
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2025-12-31 12:04:52 +0900

    2025-12-31 연습문제 (다음 큰 숫자)

---
+++ 2025-12-31 연습문제 (다음 큰 숫자)/2025-12-31 연습문제 (다음 큰 숫자)/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+int bitCount(int n)
+{
+	int cnt = 0;
+	int test = 1;
+
+	while (test <= n)
+	{
+		if (n & test)
+			++cnt;
+		test <<= 1;
+	}
+
+	return cnt;
+}
+
+int solution(int n)
+{
+	int nBit = bitCount(n);
+	int bit = 0;
+
+	while (nBit != bit)
+	{
+		bit = bitCount(++n);
+	}
+
+	return n;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(15);
+
+	return 0;
+}
