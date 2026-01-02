c726029f5e6d06166daa665de06e7374b12280ad
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-01-02 10:54:48 +0900

    2026-01-01 연습문제 (숫자의 표현)

---
+++ 2026-01-01 연습문제 (숫자의 표현)/2026-01-01 연습문제 (숫자의 표현)/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n)
+{
+	int answer = 0;
+	int num = n;
+	int sum = 0;
+	int pivot = n;
+	
+	while (num > 0)
+	{
+		sum += num;
+
+		if (sum >= n)
+		{
+			if (sum == n)
+				++answer;
+
+			num = pivot;
+			sum = 0;
+			pivot = num - 1;
+		}
+
+		--num;
+	}
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(15);
+
+	return 0;
+}
