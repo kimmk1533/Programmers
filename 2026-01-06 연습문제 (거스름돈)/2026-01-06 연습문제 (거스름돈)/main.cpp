519460bc779df40673e8d27c4a67b22d0a6a4096
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-01-06 20:36:55 +0900

    2026-01-06 연습문제 (거스름돈)

---
+++ 2026-01-06 연습문제 (거스름돈)/2026-01-06 연습문제 (거스름돈)/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+int dp[100001];
+
+int solution(int n, vector<int> money)
+{
+	dp[0] = 1;
+
+	for (int i = 0; i < money.size(); ++i)
+		for (int j = money[i]; j <= n; ++j)
+			dp[j] += dp[j - money[i]];
+
+	return dp[n] % 1000000007;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(5, { 1, 2, 5 });
+
+	return 0;
+}
