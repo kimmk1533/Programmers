0f580a6fa450a841d0b1866586c79f69e5008dab
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-01-07 19:31:08 +0900

    2026-01-07 2019 카카오 개발자 겨울 인턴십 (불량 사용자)

---
+++ 2026-01-07 2019 카카오 개발자 겨울 인턴십 (불량 사용자)/2026-01-07 2019 카카오 개발자 겨울 인턴십 (불량 사용자)/main.cpp
@@ -0,0 +1,66 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+bool isSame(const string& user, const string& bannedId)
+{
+	if (user.size() != bannedId.size())
+		return false;
+
+	for (int i = 0; i < user.size(); ++i)
+	{
+		if (bannedId[i] == '*')
+			continue;
+		if (user[i] != bannedId[i])
+			return false;
+	}
+
+	return true;
+}
+
+set<vector<int>> answer;
+
+void dfs(const vector<vector<int>>& db, vector<int> v, int level)
+{
+	if (level == db.size())
+	{
+		sort(v.begin(), v.end());
+		if (unique(v.begin(), v.end()) == v.end())
+			answer.insert(v);
+		return;
+	}
+
+	for (int i = 0; i < db[level].size(); ++i)
+	{
+		v.push_back(db[level][i]);
+		dfs(db, v, level + 1);
+		v.pop_back();
+	}
+}
+
+int solution(vector<string> user_id, vector<string> banned_id)
+{
+	const int banSize = banned_id.size();
+
+	vector<vector<int>> db(banSize);
+	for (int i = 0; i < banSize; ++i)
+		for (int j = 0; j < user_id.size(); ++j)
+			if (isSame(user_id[j], banned_id[i]) == true)
+				db[i].push_back(j);
+
+	dfs(db, {}, 0);
+	return answer.size();
+}
+
+#include <iostream>
+
+int main()
+{
+	//cout << solution({ "frodo", "fradi", "crodo", "abc123", "frodoc" }, { "fr*d*", "abc1**" }) << endl;
+	cout << solution({ "frodo", "fradi", "crodo", "abc123", "frodoc" }, { "*rodo", "*rodo", "******" }) << endl;
+	cout << solution({ "frodo", "fradi", "crodo", "abc123", "frodoc" }, { "fr*d*", "*rodo", "******", "******" }) << endl;
+
+	return 0;
+}
