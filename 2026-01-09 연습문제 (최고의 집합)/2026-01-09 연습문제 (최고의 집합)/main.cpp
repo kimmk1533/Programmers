be09020640b2c50f3c95488b2d1575c5172ae73b
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-01-10 00:20:27 +0900

    2026-01-09 연습문제 (최고의 집합)

---
+++ 2026-01-09 연습문제 (최고의 집합)/2026-01-09 연습문제 (최고의 집합)/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, int s)
+{
+	if (n > s)
+		return { -1 };
+
+	const int m = s % n;
+	vector<int> answer(n - m, s / n);
+	answer.insert(answer.end(), m, s / n + 1);
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	for (auto answer : solution(2, 9))
+		cout << answer << " ";
+	cout << endl;
+
+	for (auto answer : solution(2, 1))
+		cout << answer << " ";
+	cout << endl;
+
+	for (auto answer : solution(2, 8))
+		cout << answer << " ";
+	cout << endl;
+
+	return 0;
+}
