e0e0f8791662b9d7f52e1c91a944e0c90bd7b00d
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-01-14 15:46:24 +0900

    2026-01-13 SummerWinter Coding(~2018) (숫자 게임)

---
+++ 2026-01-13 SummerWinter Coding(~2018) (숫자 게임)/2026-01-13 SummerWinter Coding(~2018) (숫자 게임)/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+	priority_queue<int, vector<int>, greater<int>> A_PQ(A.begin(), A.end());
+	priority_queue<int, vector<int>, greater<int>> B_PQ(B.begin(), B.end());
+
+	int answer = 0;
+	while (A_PQ.empty() == false)
+	{
+		int a = A_PQ.top(); A_PQ.pop();
+		while (B_PQ.empty() == false)
+		{
+			int b = B_PQ.top(); B_PQ.pop();
+			if (a < b)
+			{
+				++answer;
+				break;
+			}
+		}
+	}
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution({ 5, 1, 3, 7, }, { 2, 2, 6, 8 }) << endl;
+	cout << solution({ 2, 2, 2, 2, }, { 1, 1, 1, 1 }) << endl;
+	cout << solution({ 1, 3, 9, 9, }, { 2, 2, 8, 10 }) << endl;
+
+	return 0;
+}
