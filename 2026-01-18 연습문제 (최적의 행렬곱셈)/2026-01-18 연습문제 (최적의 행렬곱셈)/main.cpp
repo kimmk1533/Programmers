06044b69193748c7dd0f392c423817b13926d1c3
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-01-18 23:10:52 +0900

    2026-01-18 연습문제 (최적의 행렬곱셈)

---
+++ 2026-01-18 연습문제 (최적의 행렬곱셈)/2026-01-18 연습문제 (최적의 행렬곱셈)/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <vector>
+using namespace std;
+
+vector<vector<int>> dp;
+
+int solution(vector<vector<int>> matrix)
+{
+	const int n = matrix.size();
+	dp.assign(n, vector<int>(n, 2000000000));
+	for (int i = 0; i < n; ++i)
+		dp[i][i] = 0;
+
+	for (int cnt = 1; cnt < n; ++cnt)
+	{
+		for (int s = 0; s < n - cnt; ++s)
+		{
+			int e = s + cnt;
+
+			for (int k = s; k < e; ++k)
+			{
+				dp[s][e] = min(dp[s][e], dp[s][k] + dp[k + 1][e] + (matrix[s][0] * matrix[k][1] * matrix[e][1]));
+			}
+		}
+	}
+
+	return dp[0][n - 1];
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution({ { 9, 10 }, { 10, 9 }, { 9, 1 }, { 1, 2 } });
+
+	return 0;
+}
