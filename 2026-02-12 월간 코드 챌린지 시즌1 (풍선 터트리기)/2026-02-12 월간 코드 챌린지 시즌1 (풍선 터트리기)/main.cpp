dc2131b0ef8444302c9ced2c8198fb558255b67c
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-02-12 05:51:41 +0900

    2026-02-12 월간 코드 챌린지 시즌1 (풍선 터트리기)

---
+++ 2026-02-12 월간 코드 챌린지 시즌1 (풍선 터트리기)/2026-02-12 월간 코드 챌린지 시즌1 (풍선 터트리기)/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> a)
+{
+	int answer = 0, minValue = a[a.size() - 1];
+	int minIndex = distance(a.begin(), min_element(a.begin(), a.end()));
+
+	vector<int> minVec(a.size());
+	for (int i = 0; i <= minIndex; ++i)
+		minVec[i] = a[minIndex];
+	for (int i = a.size() - 1; i > minIndex; --i)
+		minVec[i] = minValue = min(a[i], minValue);
+
+	minValue = a[0];
+	for (int i = 0; i < a.size(); ++i)
+	{
+		int left = minValue = min(minValue, a[i]);
+		int right = minVec[i];
+
+		if (!(left < a[i] && right < a[i]))
+			++answer;
+	}
+
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution({ 9, -1, -5 }) << endl;
+	cout << solution({ -16, 27, 65, -2, 58, -92, -71, -68, -61, -33 }) << endl;
+
+	return 0;
+}
