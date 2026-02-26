e8bf8c8fa584374a083e2b512e637a12eb6ea440
Ceeu <55845465+kimmk1533@users.noreply.github.com>
2026-02-26 17:00:48 +0900

    2026-02-22 2021 카카오 채용연계형 인턴십 (시험장 나누기)

---
+++ 2026-02-22 2021 카카오 채용연계형 인턴십 (시험장 나누기)/2026-02-22 2021 카카오 채용연계형 인턴십 (시험장 나누기)/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+
+using namespace std;
+
+int solution(int k, vector<int> num, vector<vector<int>> links)
+{
+	int answer = 0;
+	return answer;
+}
+
+#include <iostream>
+
+int main()
+{
+	cout << solution(3, { 12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1 }, { {-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {8, 5}, {2, 10}, {3, 0}, {6, 1}, {11, -1}, {7, 4}, {-1, -1}, {-1, -1} }) << endl;
+	cout << solution(1, { 6, 9, 7, 5 }, { {-1, -1}, {-1, -1}, {-1, 0}, {2, 1} }) << endl;
+	cout << solution(2, { 6, 9, 7, 5 }, { {-1, -1}, {-1, -1}, {-1, 0}, {2, 1} }) << endl;
+	cout << solution(4, { 6, 9, 7, 5 }, { {-1, -1}, {-1, -1}, {-1, 0}, {2, 1} }) << endl;
+
+	return 0;
+}
